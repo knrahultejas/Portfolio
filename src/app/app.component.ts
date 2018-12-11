2bad7681e524b4a54a87e1a03c67077ef0ad2b87
Mona <knrahultejas@gmail.com>
2018-12-11 12:33:23 -0600

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'My Portfolio';
+
+  message1 = 'An amusement as a 3 years old, using the computer to play games like Dave and Chess using Command Prompt is a dream come true working as a Software Engineer in this evolved "Computer Science world". It has been a journey of a lot of errors and debugging, puzzles and brilliance working as a team.';
+  message2 = 'When I am not working, I love to sing, watch soccer, play basketball, travel whenever I can and count stars at night. I love doing adventures like skydiving and trekking.';
+  message3 = 'I am a team player who loves meeting new people and learn new things.';
+  message4 = 'Full-Stack Developer Specialties:';
+  message5 = 'Development: Java, JDBC, UML, SQL, C, C++';
+  message6 = 'Front-End Development: HTML5, CSS3, JavaScript, jQuery, Angular, Typescript, Node.js, JSP, Ajax, Bootstrap';
+  message7 = 'Cloud Technologies: AWS';
+  message8 = 'Databases: MySQL Server, SQL Server, MongoDB';
+  message9 = 'Build Tools: Maven, Gradle';
+  message10 = 'Version Control: GitHub';
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ExperienceComponent,
+    FeedbackComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
